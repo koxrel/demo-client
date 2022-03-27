5e242ade5eb32e8eaaccdf81e243de690a4c75ff
koxrel <15571888+koxrel@users.noreply.github.com>
2022-03-27 15:31:47 +0300

    initial commit

--- src/main/kotlin/com/app/ClientLoadTesting.kt
+++ src/main/kotlin/com/app/ClientLoadTesting.kt
@@ -1,6 +1,7 @@
 package com.app
 
 import com.app.client.InsuranceCalculationWsClient
+import io.micronaut.context.annotation.Prototype
 import io.micronaut.context.event.ApplicationEventListener
 import io.micronaut.discovery.event.ServiceReadyEvent
 import io.micronaut.http.client.annotation.Client
@@ -10,19 +11,15 @@ import reactor.core.Disposable
 import reactor.core.publisher.Flux
 
 @Singleton
-class ClientLoadTesting(@Client("http://localhost:8080") val webSocketClient: WebSocketClient): ApplicationEventListener<ServiceReadyEvent> {
+open class ClientLoadTesting(@Client("http://10.129.0.11:8080") val webSocketClient: WebSocketClient) : ApplicationEventListener<ServiceReadyEvent> {
     override fun onApplicationEvent(event: ServiceReadyEvent?) {
         val clients = mutableListOf<Disposable?>()
 
         for (i in 1..180) {
             for (j in 1..15_000) {
-                try {
-                    val client = Flux.from(webSocketClient.connect(InsuranceCalculationWsClient::class.java, "/ws/failing/calculation")).subscribe { client -> client.send("") }
+                val client = Flux.from(webSocketClient.connect(InsuranceCalculationWsClient::class.java, "/ws/failing/calculation")).subscribe { client -> client.send("") }
 
-                    clients.add(client)
-                } catch (e: Exception) {
-                    e
-                }
+                clients.add(client)
             }
             Thread.sleep(1_000)
         }
