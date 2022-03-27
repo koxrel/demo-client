6f1dcadd5ba81370f27ff08b1dd316ac2c3acd2e
koxrel <15571888+koxrel@users.noreply.github.com>
2022-03-27 23:12:22 +0300

    initial commit

--- src/main/kotlin/com/app/ClientLoadTesting.kt
+++ src/main/kotlin/com/app/ClientLoadTesting.kt
@@ -7,20 +7,26 @@ import io.micronaut.discovery.event.ServiceReadyEvent
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.websocket.WebSocketClient
 import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
 import reactor.core.Disposable
 import reactor.core.publisher.Flux
 
 @Singleton
-open class ClientLoadTesting(@Client("http://10.129.0.11:8080") val webSocketClient: WebSocketClient) : ApplicationEventListener<ServiceReadyEvent> {
+open class ClientLoadTesting(@Client("http://10.129.0.29:8080") val webSocketClient: WebSocketClient) : ApplicationEventListener<ServiceReadyEvent> {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun onApplicationEvent(event: ServiceReadyEvent?) {
         val clients = mutableListOf<Disposable?>()
+        var currentNumber = 0L
 
         for (i in 1..180) {
             for (j in 1..15_000) {
                 val client = Flux.from(webSocketClient.connect(InsuranceCalculationWsClient::class.java, "/ws/failing/calculation")).subscribe { client -> client.send("") }
 
                 clients.add(client)
+                ++currentNumber
             }
+            log.info("Current clients: $currentNumber")
             Thread.sleep(1_000)
         }
     }
